b575787c644e39e56380c4f67e23d9b93a57df6a
root <root@kyn-prod-app-01-vm-eastus>
2023-04-03 12:02:32 +0000

    check_source - terraform template updated

---
+++ check_source_641856c596d0913785fc93a5_63407.tf
@@ -0,0 +1,14 @@
+module "check_source_641856c596d0913785fc93a5_63407" {
+  source                        = "git::https://github.com/seshanb/test/AzureRM_VM_Provision_Ubuntu_14_04_Latest.git"
+  location                      = "East US"
+  azure_Virtual_Network_Name    = "testVirtualNetwork"
+  address_Space                 = ["10.0.0.0/16"]
+  azure_Subnet_Name             = "Subnet1"
+  address_Prefix                = ["10.0.2.0/24"]
+  azure_Network_Interface_Name  = "testnetworkinterface1"
+  IP_Configuration_Name         = "testconfiguration-private-ip"
+  private_IP_Address_Allocation = "Dynamic"
+  prefix                        = "testazure"
+  tags                          = { "key1" : "value123" }
+  azure_Resource_Group          = "QA_INFRA_RG"
+}
