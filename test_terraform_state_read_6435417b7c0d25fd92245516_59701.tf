3d8acaf794103302cc07e971c54daa1ec916b123
root <root@kyn-prod-app-01-vm-eastus>
2023-04-20 06:43:14 +0000

    test_terraform_state_read - terraform template updated

---
+++ test_terraform_state_read_6435417b7c0d25fd92245516_59701.tf
@@ -0,0 +1,5 @@
+module "test_terraform_state_read_6435417b7c0d25fd92245516_59701" {
+  source               = "git::https://github.com/githlearner/gith_terr/readtf.git?ref=main"
+  azure_Resource_Group = "TestRG"
+  location             = "East US"
+}
